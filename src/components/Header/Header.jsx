1fdcf95c7e06ecd662d072e17d76478c74708b03
emonShimoul <emonshimoul@gmail.com>
2025-01-30 18:48:05 +0600

    added some more styles in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,18 @@ import NavBar from "../NavBar/NavBar";
 
 const Header = () => {
   return (
-    <div className="bg-[#9538E2] rounded-2xl pb-20">
+    <div className="bg-[#9538E2] rounded-2xl pb-40">
       <NavBar></NavBar>
-      <div className="text-center text-white p-24 flex flex-col gap-8">
-        <h2 className="text-6xl font-bold w-5/6 mx-auto">
+      <div className="text-center text-white flex flex-col gap-8 pt-16">
+        <h2 className="text-6xl font-bold w-4/5 mx-auto leading-18">
           Upgrade Your Tech Accessorize with Gadget Heaven Accessories
         </h2>
-        <p>
+        <p className="w-1/2 mx-auto">
           Explore the latest gadgets that will take your experience to the next
           level. From smart devices to the coolest accessories, we have it all!
         </p>
         <div>
-          <button className="bg-white text-[#9538E2] font-bold py-2 px-6 rounded-4xl">
+          <button className="bg-white text-[#9538E2] font-bold py-2 px-6 rounded-4xl cursor-pointer">
             Shop Now
           </button>
         </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,39 +15,41 @@ const NavBar = () => {
     </>
   );
   return (
-    <div className="navbar bg-[#9538E2] text-white mt-8 rounded-2xl">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="bg-[#9538E2] text-white mt-8 rounded-2xl pt-4">
+      <div className="navbar max-w-[1280px] mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              {links}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            {links}
-          </ul>
+          <p className="text-xl font-bold">Gadget Heaven</p>
         </div>
-        <a className="btn btn-ghost text-xl">Gadget Heaven</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
       </div>
     </div>
   );
