604853fa39d91f3ebbaae7061e75c08e9e37ccd8
emonShimoul <emonshimoul@gmail.com>
2025-01-30 18:37:34 +0600

    style the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,10 @@ import NavBar from "../NavBar/NavBar";
 
 const Header = () => {
   return (
-    <div className="bg-[#9538E2]">
+    <div className="bg-[#9538E2] rounded-2xl pb-20">
       <NavBar></NavBar>
       <div className="text-center text-white p-24 flex flex-col gap-8">
-        <h2 className="text-6xl">
+        <h2 className="text-6xl font-bold w-5/6 mx-auto">
           Upgrade Your Tech Accessorize with Gadget Heaven Accessories
         </h2>
         <p>
@@ -13,7 +13,7 @@ const Header = () => {
           level. From smart devices to the coolest accessories, we have it all!
         </p>
         <div>
-          <button className="bg-white text-[#9538E2] py-2 px-6 rounded-4xl">
+          <button className="bg-white text-[#9538E2] font-bold py-2 px-6 rounded-4xl">
             Shop Now
           </button>
         </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-[#9538E2] text-white mt-8 rounded-2xl">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
