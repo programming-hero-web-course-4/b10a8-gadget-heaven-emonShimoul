8d3660b91b2528ae8ef60f503d530489b18d9146
emonShimoul <emonshimoul@gmail.com>
2025-01-30 20:03:00 +0600

    added banner image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import NavBar from "../NavBar/NavBar";
+import BannerImage from "../../assets/banner.jpg";
 
 const Header = () => {
   return (
-    <div className="bg-[#9538E2] rounded-2xl pb-40">
+    <div className="bg-[#9538E2] rounded-2xl">
       <NavBar></NavBar>
-      <div className="text-center text-white flex flex-col gap-8 pt-16">
+      <div className="text-center text-white flex flex-col gap-8 pt-16 pb-56">
         <h2 className="text-6xl font-bold w-4/5 mx-auto leading-18">
           Upgrade Your Tech Accessorize with Gadget Heaven Accessories
         </h2>
@@ -17,6 +18,9 @@ const Header = () => {
             Shop Now
           </button>
         </div>
+      </div>
+      <div className="max-w-[1065px] absolute">
+        <img src={BannerImage} alt="" />
       </div>
     </div>
   );
