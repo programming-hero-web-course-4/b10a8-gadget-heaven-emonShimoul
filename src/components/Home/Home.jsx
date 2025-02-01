ec8c6fed8061c62a3564d9874d0cc7f868a273fc
emonShimoul <emonshimoul@gmail.com>
2025-02-01 23:57:19 +0600

    start the design of products in home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,26 @@
+import { NavLink } from "react-router-dom";
 import Product from "../Product/Product";
 
 const Home = () => {
   return (
     <div className="mt-[500px] mb-16">
-      <h2 className="text-center font-bold text-4xl">
+      <h2 className="text-center font-bold text-4xl mb-20">
         Explore Cutting-Edge Gadgets
       </h2>
-      <Product></Product>
+      <div className="grid grid-cols-2">
+        <div>
+          <NavLink>All Products</NavLink>
+          <NavLink>Laptops</NavLink>
+          <NavLink>Phones</NavLink>
+          <NavLink>Accessories</NavLink>
+          <NavLink>SmartWatches</NavLink>
+          <NavLink>MacBook</NavLink>
+          <NavLink>Iphone</NavLink>
+        </div>
+        <div>
+          <Product></Product>
+        </div>
+      </div>
     </div>
   );
 };
