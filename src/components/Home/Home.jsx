d401e2c238c9cc9b224892108681d2621a78b3a4
emonShimoul <emonshimoul@gmail.com>
2025-02-01 23:03:57 +0600

    added product card

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Product from "../Product/Product";
+
 const Home = () => {
   return (
     <div className="mt-[500px] mb-16">
       <h2 className="text-center font-bold text-4xl">
         Explore Cutting-Edge Gadgets
       </h2>
+      <Product></Product>
     </div>
   );
 };
